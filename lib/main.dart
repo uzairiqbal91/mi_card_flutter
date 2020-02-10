c6da09b8bda2adad7c4823af4d3488cea3a745d6
uzair <uzair@fomarkmedia.com>
2020-02-10 21:33:00 +0500

    made stateless widget and added some comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
+// for hot reload we have to change flutter stateless or statefull widget
+// difference between hot reload and hot restart
+// hot restart : reset the  state of your app , if we you need the test from the test this will use
+// hot reload : only add the changes
+
+
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+// this code is building stateless widget
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
+
+
+
 }
